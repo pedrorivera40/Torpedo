64d45a67a6b4bc832a9625b663db0a8db6a316f2
diego-amador <diego.amador@upr.edu>
2020-05-02 16:06:39 -0400

    - changed private methods, to be inside their respective classes. - Added Node creation aswell as Edge creation to the graph input method

--- TorpedoBackend/Agent/graph.py
+++ TorpedoBackend/Agent/graph.py
@@ -1,120 +1,137 @@
 # Reading an excel file using Python 
 import xlrd 
 
-def get_destination(self):
-    """
-    Private method to return destination Node 
-    
-    :param self:
-    :returns destination:
-    :type destination: Node
-    """
-    return self.destination
-
-def get_distance(self):
-    """
-    Private method to return distance
-    
-    :param self:
-    :returns distance:
-    :type distance: float
-    """
-    return self.distance
-
-def get_speed_limit(self):
-    """
-    Private method to return speed_limit
-    
-    :param self:
-    :returns speed_limit:
-    :type speed_limit: int
-    """
-    return self.speed_limit
-
-def get_traffic_delay(self):
-    """
-    Private method to return traffic_delay
-    
-    :param self:
-    :returns traffic_delay:
-    :type traffic_delay: float
-    """
-    return self.traffic_delay
 
 
 class Node:
     """
     Node class
     """
-    def Node(self,city,edges,heuristic_value):
+  
+    def __init__(self,city,edges,heuristic_value,previous):
         """
         Node constructor
     
         :param city: 
         :type city: String
+        :param previous: 
+        :type previous: Node
         :param edges:
         :type edges: List<Edge>
         :param heuristic_value:
         :type heuristic_value: Float
         """
+        self.previous = None
         self.city = city
         self.edges= edges
         self.heuristic_value = heuristic_value
-        return self
+     
+    def get_city(self):
+        """
+        Private method to return Node city name
     
- 
+        :param self:
+        :returns city:
+        :type city: string
+        """
+        return self.city
 
-
-def get_city(self):
-    """
-    Private method to return Node city name
+    def get_edges(self):
+        """
+        Private method to return edges list
     
-    :param self:
-    :returns city:
-    :type city: string
-    """
-    return self.city
+        :param self:
+        :returns edges:
+        :type edges: List<Edge>
+        """
+        return self.edges
 
-def get_edges(self):
-    """
-    Private method to return edges list
+    def get_heuristic_value(self):
+        """
+        Private method to return heuristic_value
     
-    :param self:
-    :returns edges:
-    :type edges: List<Edge>
-    """
-    return self.edges
+        :param self:
+        :returns heuristic_value:
+        :type heuristic_value: float
+        """
+        return self.heuristic_value
 
-def get_heuristic_value(self):
-    """
-    Private method to return heuristic_value
+    def get_previous(self):
+        """
+        Private method to return prvious Node
     
-    :param self:
-    :returns heuristic_value:
-    :type heuristic_value: float
-    """
-    return self.heuristic_value
-
+        :param self:
+        :returns previous:
+        :type heuristic_value: Node
+        """
+        return self.previous
    
+   
+     
 class Edge:
     """
     Edge class
-    """
-    def Edge(self,destination,distance,speed_limit,traffic_delay):
+    """ 
+    def __init__(self,destination,distance,speed_limit,traffic_delay):
         """
         Edge constructor
     
         :param destination: 
+        :type destination: Node
         :param distance:
+        :tpye distance: Float
         :param speed_limit:
+        :type speed_limit: Integer
         :param traffic_delay:
         """
         self.destination = destination
         self.distance = distance
         self.speed_limit = speed_limit
         self.traffic_delay = traffic_delay
-        return self
+         
+    def get_destination(self):
+        """
+        Private method to return destination Node 
     
+        :param self:
+        :returns destination:
+        :type destination: Node
+        """
+        return self
+
+    def get_distance(self):
+        """
+        Private method to return distance
+    
+        :param self:
+        :returns distance:
+        :type distance: float
+        """
+        return self.distance
+
+    def get_speed_limit(self):
+        """
+        Private method to return speed_limit
+    
+        :param self:
+        :returns speed_limit:
+        :type speed_limit: int
+        """
+        return self.speed_limit
+
+    def get_traffic_delay(self):
+        """
+        Private method to return traffic_delay
+    
+        :param self:
+        :returns traffic_delay:
+        :type traffic_delay: float
+        """
+        return self.traffic_delay
+
+   
+    
+   
 
 
 
@@ -132,15 +149,26 @@ class GraphInput:
         wb = xlrd.open_workbook(path) 
         sheet = wb.sheet_by_index(0) 
         
-       
+        #traverse the first column to store all the nodes, with their city name.
+        #TODO: We need to calculate the heuristic_value 
+        for i in range(sheet.nrows): 
+           listOfNodes.append(Node(previous=None,city=str(sheet.cell_value(i, 0)) , edges=None, heuristic_value= 1 ))
+        
+        for node in listOfNodes:
+            print("Creating node list, adding: "+ str(node.city))
+        #After making the list of nodes, we create the edge list for each Node :
+        #traverse the first column
         for i in range(sheet.nrows): 
             nodeEdges = [] 
+            #go through each row 
             for j in range(sheet.ncols):
+                #Check each cell
                 cell = sheet.cell_value(i, j)
-                
+                #Check if cell is empty, this means the end of a edge list
                 if cell is not "" :
+                        #check if cell contains an edge
                         if cell[0] == '(' :
-                            
+                            #store the tuple information from the cell
                             res = [] 
                             temp = [] 
                             for token in cell.split(", "): 
@@ -151,22 +179,35 @@ class GraphInput:
                                         temp = []
   
                             # printing result 
-                            print('edge #'+str(j))
-                            print("\tDestination: "+str(res[0][0])+"\n\tDistance: "+str(res[0][1])+"\n\tMax Speed: "+str(res[0][2])+"\n\tTraffic Delay: "+str(res[0][3]))
+                            #print('edge #'+str(j))
+                            #print("\tDestination: "+str(res[0][0])+"\n\tDistance: "+str(res[0][1])+"\n\tMax Speed: "+str(res[0][2])+"\n\tTraffic Delay: "+str(res[0][3]))
                             #store cell information 
+                            destination =None
+                            distance=float(res[0][1])
+                            speed_limit=int(res[0][2])
+                            traffic_delay=float(res[0][3])
+                            #reference node from listOfNodes
+                            for node in listOfNodes:
+                                print(node.city+" is being compared with "+str(res[0][0]))
+                                if node.city == str(res[0][0]):
+                                    destination = node
+                            #no Node was found , could be an error in the Excel file
+                            if destination is not None:
+                                nodeEdges.append(Edge(destination=destination,distance=distance,speed_limit=speed_limit,traffic_delay=traffic_delay) )
                             
-                            destination =str(res[0][0])
-                            distance=str(res[0][1])
-                            speed_limit=str(res[0][2])
-                            traffic_delay=str(res[0][3])
-                            nodeEdges.append(Edge().Edge(destination=destination,distance=distance,speed_limit=speed_limit,traffic_delay=traffic_delay) )
-                            
-                            
+                            else:
+                                return print("Node not found "+str(res[0][0]))
+                       
+                           
                         else:
+                            #cell contains a node, we ignore this cell 
                             print("\nNode:\t"+cell)
                             print("\nEdges: ")
-            
-        listOfNodes.append(Node().Node(city=str(sheet.cell_value(i, 0)) , edges=nodeEdges, heuristic_value= 1 ))
+                
+                else:
+                    #At the end of the list we assign the edge list to the Node in the row being traversed
+                    listOfNodes[i].edges=nodeEdges
+                
        # print( listOfNodes)
         return listOfNodes 
         
